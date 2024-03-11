674dea115a917b8b6a5aea6daab63eeb80346f94
K Iceline <2604818431@qq.com>
2024-03-11 15:38:45 +0000

    pytorch

--- RPS.py
+++ RPS.py
@@ -1,5 +1,7 @@
 # The example function below keeps track of the opponent's history and plays whatever the opponent played two plays ago. It is not a very good player so you will need to change the code to pass the challenge.
 
+import torch
+
 def player(prev_play, opponent_history=[]):
     opponent_history.append(prev_play)
 
